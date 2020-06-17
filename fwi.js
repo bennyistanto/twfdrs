bb11155b0d2681800080a4672bd46619774fd2f6
Benny Istanto <bennyistanto@outlook.com>
2020-06-17 15:14:35 +0700

    Update script

--- fwi.js
+++ fwi.js
@@ -14,11 +14,13 @@ function FFMCcalc(ffmc0,prec,hum,wind,temp){
       'exp':Math.exp(),
       'sqrt':Math.sqrt(),
       'rf':prec.subtract(0.5)})})
+  
   var ed = img.expression('0.942*(pow(hum,0.679)) + (11.0*exp((hum-100.0)/10.0))+ 0.18*(21.1-temp)*(1.0 - 1.0/exp(0.1150 * hum))',{
     'pow':Math.pow(),
     'exp':Math.exp(),
     'hum':hum.select(0),
     'temp':temp.select(0)}) //*Eq. 4*#
+  
   var m = img.expression('mo<ed? mo<=ew? ew - (ew - mo)/(pow(10.0,kw1)):mo : mo==ed? mo: ed + (mo-ed)/(pow(10.0,kw2))',{
     'pow':Math.pow(),
     'mo':mo.select(0),
@@ -44,21 +46,26 @@ function FFMCcalc(ffmc0,prec,hum,wind,temp){
         'sqrt':Math.sqrt(),
         'hum':hum.select(0),
         'wind':wind.select(0)})})})
+  
   var ffmc = m.expression('(59.5 * (250.0 - b(0))) / (147.2 + b(0))')
   ffmc = ffmc.expression('b(0)>101.0? 101.0 : b(0)<=0.0? 0.0 : b(0)')
+  
   return ffmc }
 
 function DMCcalc(dmc0,mth,temp,prec,hum){
   var el = [6.5,7.5,9.0,12.8,13.9,13.9,12.4,10.9,9.4,8.0,7.0,6.0]
   var t = temp.expression('b(0)<-1.1? -1.1:b(0)')
+  
   var rk = img.expression('1.894*(temp+1.1) * (100.0-hum) * (el*0.0001)',{
     'temp':t.select(0),
     'hum':hum.select(0),
     'el':el[mth-1]})
+  
   var b = dmc0.expression('b(0)<=33.0? eq13a : b(0)<=65? eq13b : eq13c',{
     'eq13a':dmc0.expression('100.0 /(0.5 + 0.3*b(0))'),
     'eq13b':dmc0.expression('14.0 - 1.3*log(b(0))',{'log':Math.log()}),
     'eq13c':dmc0.expression('6.2 * log(b(0)) - 17.2',{'log':Math.log()})})
+  
   var pr = img.expression('prec>1.5? 43.43*(5.6348 - log(wmr-20.0)) : dmc0',{
     'log':Math.log(0),
     'prec':prec.select(0),
@@ -70,19 +77,23 @@ function DMCcalc(dmc0,mth,temp,prec,hum){
       'rw':prec.expression('0.92*b(0)-1.27'),
       'b':b.select(0)})})
   pr = pr.expression('b(0)<0?0:b(0)')
+  
   var dmc = img.expression('pr+rk',{
     'pr':pr.select(0),
     'rk':rk.select(0)})
   dmc = dmc.expression('b(0)<=1.0?1.0:b(0)')
+  
   return dmc}
 
 function DCcalc(dc0,mth,temp,prec){
   var fl = [-1.6, -1.6, -1.6, 0.9, 3.8, 5.8, 6.4, 5.0, 2.4, 0.4, -1.6, -1.6]
   var t = temp.expression('b(0)<-2.8?-2.8:b(0)')
+  
   var pe = t.expression('(0.36*(t+2.8)+fl)/2',{
     't':t.select(0),
     'fl':fl[mth-1]})
   pe = pe.expression('b(0)<=0.0?0.0:b(0)')
+  
   var dc = img.expression('prec>2.8? dr>0.0? dr+pe: pe : dc0+pe',{
     'prec':prec.select(0),
     'pe':pe.select(0),
@@ -97,12 +108,15 @@ function DCcalc(dc0,mth,temp,prec){
   
 function ISIcalc(ffmc,wind){
   var mo = ffmc.expression('147.2*(101.0-b(0))/(59.5+b(0))') //#*Eq. 1*#
+  
   var ff = mo.expression('19.115*exp(b(0)*-0.1386)*(1.0+(pow(b(0),5.31))/49300000.0)',{
     'exp':Math.exp(),
     'pow':Math.pow()}) //*Eq. 25*#
+  
   var isi = ff.expression('b(0)*exp(0.05039*wind)',{
     'exp':Math.exp(),
     'wind':wind.select(0)}) //*Eq. 26*#
+  
   return isi}
 
 function BUIcalc(dmc,dc){
